94e8ea13e9e419daa2b8ba49100dd3c2b71b70d4
okamuuu <okamuuu@gmail.com>
2025-06-07 20:51:09 +0900

    feat: ec2

--- terraform_beginner/04-terraform-aws-ec2-demo/main.tf
+++ terraform_beginner/04-terraform-aws-ec2-demo/main.tf
@@ -3,8 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo" {
-  ami           = "ami-0df99b3a8349462c1" # Amazon Linux 2（東京リージョン）
+  ami           = "ami-0da9a208c352ca999" # 2025年06月
   instance_type = "t2.micro"
+  key_name      = "okamuuu-dev"
 
   tags = {
     Name = "terraform-ec2-demo"
